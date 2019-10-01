7aef6d7224b6a6e1c2be3c27451f4be2860f9e69
tarotaro0 <kodai.nakamura@mercari.com>
2019-10-01 18:05:51 +0900

    Add foo service

---
+++ services/foo/go.mod
@@ -0,0 +1,3 @@
+module github.com/tarotaro0/exp-mm-repo/services/foo
+
+go 1.13
---
+++ services/foo/main.go
@@ -0,0 +1,8 @@
+package foo
+
+import "fmt"
+
+// HelloFoo say "Hello, Foo"
+func HelloFoo() {
+	fmt.Println("Hello, Foo v1.0.0")
+}
