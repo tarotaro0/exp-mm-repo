3f4b6f550f5153e1a79ca05f92911f95306a051b
tarotaro0 <kodai.nakamura@mercari.com>
2019-10-02 13:14:52 +0900

    Import super log system to foo-service

--- services/foo/go.mod
+++ services/foo/go.mod
@@ -1,3 +1,5 @@
 module github.com/tarotaro0/exp-mm-repo/services/foo
 
 go 1.13
+
+require github.com/tarotaro0/exp-mm-repo v1.0.1
--- services/foo/main.go
+++ services/foo/main.go
@@ -1,8 +1,13 @@
 package foo
 
-import "fmt"
+import (
+	"fmt"
+
+	superlog "github.com/tarotaro0/exp-mm-repo/conf"
+)
 
 // HelloFoo say "Hello, Foo"
 func HelloFoo() {
 	fmt.Println("Hello, Foo v1.1.0")
+	superlog.SuperLog("Foo")
 }
